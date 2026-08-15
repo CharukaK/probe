7cd57767c34d6a869b1c5ca58fc4030e8fce9b0f
CharukaK <charukakarunanayake@gmail.com>
2026-08-15 20:28:00 +0530

    Integrate with ts bindings

--- cli/cmd/main.go
+++ cli/cmd/main.go
@@ -1,8 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	ts_probe "github.com/charukak/probe/bindings/go"
+	ts "github.com/tree-sitter/go-tree-sitter"
+)
 
 func main() {
-	fmt.Println("Yo")
-}
+	parser := ts.NewParser()
+	defer parser.Close()
 
+	parser.SetLanguage(ts.NewLanguage(ts_probe.Language()))
+
+	source, err := os.ReadFile("./hello.probe")
+
+	if err != nil {
+		return
+	}
+
+
+	// Parse the string into an Abstract Syntax Tree (AST)
+	tree := parser.Parse(source, nil)
+	if tree == nil {
+		panic("Failed to parse code.")
+	}
+
+	// CRITICAL: Manually close the tree when finished
+	defer tree.Close()
+
+	// Retrieve the root node of the syntax tree
+	rootNode := tree.RootNode()
+
+	// Output the S-expression format of the AST
+	fmt.Println("AST Structure:")
+	fmt.Println(rootNode.ToSexp())
+}
